91ea5dcef7d581d41f5a4af09ad775c099a400de
xujian-lele <648909477@qq.com>
2025-02-07 11:28:22 +0800

    update code

--- src/Sample/fm_sample_from_df_test.cpp
+++ src/Sample/fm_sample_from_df_test.cpp
@@ -1,5 +1,5 @@
-#ifndef DF_H_
-#define DF_H_
+#ifndef FM_SAMPLE_FROM_TXT_TEST_H_
+#define FM_SAMPLE_FROM_TXT_TEST_H_
 
 #include <iostream>
 #include <vector>
@@ -10,17 +10,16 @@
 #include <numeric>
 #include <iterator>
 #include <functional>
-#include "fm_sample_from_df.h"
+#include "fm_sample_from_txt.h"
 
 using namespace std;
 int main() {
-    // 存储 sample 数据
     std::vector<std::string> samples = {
-        "1\0012\00210\00220\00230\00240",
-        "3\0014\00211\00221\00231\00241",
+        "1\0021\0012\00210\00220\00230\00240",
+        "0\0023\0014\00211\00221\00231\00241",
     };
     for (const string& line: samples) {
-        fm_sample sample(line);
+        fm_sample_from_txt sample(line);
         cout << sample.y << endl;
         for (const auto& key: sample.x) {
             cout << key.first << " " << key.second << endl;
@@ -28,4 +27,4 @@ int main() {
     }
     return 0;
 }
-#endif /*DF_H_*/
+#endif /*FM_SAMPLE_FROM_TXT_TEST_H_*/
